cc1ff22f8a3028944c4ead3a7ca874a98204df25
Jacob Strength <jstrength562@gmail.com>
2024-12-19 10:53:22 -0700

    day19 part2

--- 2024/day19/part2/main.py
+++ 2024/day19/part2/main.py
@@ -1,29 +1,29 @@
+from collections import defaultdict
+
 raw_towel_patterns, raw_designs = open("big_input.txt").read().split("\n\n")
 
 towel_patterns = raw_towel_patterns.split(", ")
 designs = raw_designs.strip().split("\n")
 
-cache = {}
-def can_sort(design, idx):
+cache = defaultdict(int)
+def count_sort(design, idx):
     if design[idx:] in cache:
         return cache[design[idx:]]
-    cache[design[:idx]] = True
 
+    count = 0
     if idx == len(design):
-        return True
+        return 1
     for pattern in towel_patterns:
         if design[idx:idx+len(pattern)] == pattern:
-            if can_sort(design, idx+len(pattern)):
-                return True
-    cache[design[idx:]] = False
-    return False
+            count += count_sort(design, idx+len(pattern))
+    cache[design[idx:]] = count
+    return count
 
-count = 0
+total_count = 0
 for design in designs:
     #print(design)
-    if can_sort(design, 0):
-        count += 1 
+    total_count += count_sort(design, 0)
     #print()
 
 #print(cache)
-print(count)
+print(total_count)
